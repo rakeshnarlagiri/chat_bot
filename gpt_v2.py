b853ae1ffec324b957b36b0e60793878e509b357
rakeshnarlagiri <133942927+rakeshnarlagiri@users.noreply.github.com>
2024-05-31 15:45:48 +0530

    Update gpt_v2.py

--- gpt_v2.py
+++ gpt_v2.py
@@ -1,24 +1,25 @@
 import os
 import time
-
-import streamlit as st
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory, StreamlitChatMessageHistory
-from langchain.chains import ConversationalRetrievalChain, LLMChain
-from langchain.vectorstores import Chroma
-from langchain_core.prompts import PromptTemplate
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import CharacterTextSplitter
-import chromadb
-from utils import get_available_openai_models
 import tempfile
-from streaming import StreamHandler
+import json
+
 import pandas as pd
 import chardet
 from io import StringIO
 from docx import Document
-import json
+
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory, StreamlitChatMessageHistory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import Chroma
+from langchain_core.prompts import PromptTemplate
+
+from utils import get_available_openai_models
+from streaming import StreamHandler
 
 os.environ["OPENAI_API_KEY"] = "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"
 DB_DIRECTORY = "./chroma"
@@ -79,7 +80,7 @@ class StreamlitChatView:
             with st.expander("Prompts"):
                 self.context_prompt = st.text_area("Context prompt", value=TEMPLATE)
             self.input_file = st.file_uploader("Upload File", type=["pdf", "docx", "doc", "json", ".xlsx"])
-            self.selected_db = st.sidebar.selectbox("Select saved database:", [""] + get_saved_databases(), index=0)
+            self.selected_db = st.selectbox("Select saved database:", [""] + get_saved_databases(), index=0)
             self.csv_file = st.file_uploader("Upload csv for questions", type="csv")
 
         self.user_query = st.chat_input(placeholder="Ask me anything!")
@@ -96,22 +97,12 @@ class StreamlitChatView:
 def read_csv(view):
     if view.csv_file:
         file_content = view.csv_file.read()
-
-        # Detect the encoding of the file content
         result = chardet.detect(file_content)
         encoding = result['encoding']
-
-        # Convert the byte content to a string buffer using the detected encoding
         decoded_content = file_content.decode(encoding)
         file_buffer = StringIO(decoded_content)
-
-        # Read the CSV content from the string buffer
         df = pd.read_csv(file_buffer)
-
         return df
-    # for index, row in df.iterrows():
-    #     value = df.iloc[index, 0]
-    #     print(value)
 
 
 def clear_chat(memory):
@@ -132,100 +123,35 @@ def prepare_docs(view):
             data.append(paragraph.text)
         docs = []
         for text in data:
-            document = Document()  # Initialize a document object
+            document = Document()
             document.page_content = text
-            document.metadata = {}  # Assign text content to the page_content attribute
+            document.metadata = {}
             docs.append(document)
         return docs
     elif file_extension.lower() == '.json':
-
-        # Open the JSON file and read its contents
         with open(file_path, 'r') as json_file:
-            # Load the JSON data
             data = json.load(json_file)
-
         docs = []
         for text in data:
-            document = Document()  # Initialize a document object
+            document = Document()
             document.page_content = text
-            document.metadata = {}  # Assign text content to the page_content attribute
+            document.metadata = {}
             docs.append(document)
         return docs
-
     elif file_extension.lower() == '.xlsx':
         data = pd.read_excel(file_path)
         docs = []
         for text in data:
-            document = Document()  # Initialize a document object
+            document = Document()
             document.page_content = text
-            document.metadata = {}  # Assign text content to the page_content attribute
+            document.metadata = {}
             docs.append(document)
         return docs
     else:
         raise ValueError("Unsupported file type")
 
 
-def main():
-    view = StreamlitChatView()
-
-    # prompt = PromptTemplate.from_template(view.context_prompt)
-    prompt = PromptTemplate(
-        input_variables=["chat_history", "context", "question"],
-        template=view.context_prompt, )
-
-    msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, output_key='answer',
-                                      return_messages=True)
-
-    df = read_csv(view)
-    if view.input_file:
-        data = prepare_docs(view)
-
-    col1, col2 = st.columns([6, 1])
-
-    with col1:
-        st.header(f"Chat with Docs using OpenAI")
-
-    with col2:
-        if st.button("Clear ↺"):
-            clear_chat(memory)
-
-    if view.input_file and not view.selected_db:
-        process_pdf(view, memory, prompt, df, data)
-    elif view.selected_db and view.selected_db != "" and not view.input_file:
-        process_pdf(view, memory, prompt, df)
-    elif view.selected_db and view.input_file:
-        st.warning("select only one source( Either upload or select db )", icon="⚠️")
-    else:
-        # st.success("upload or select db to chat with")
-        st.info('upload file or select db to chat with', icon="ℹ️")
-
-
-def process_pdf(view, memory, prompt, df, data=None):
-    embeddings = OpenAIEmbeddings(model=view.embeddings_model_name)
-    if data:
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        docs = text_splitter.split_documents(data)
-        if os.path.exists(os.path.join(DB_DIRECTORY, view.input_file.name)):
-            st.warning('already have a db for this', icon="⚠️")
-        else:
-            vectorstore = Chroma.from_documents(docs, embeddings, persist_directory=os.path.join(DB_DIRECTORY,
-                                                                                                 view.input_file.name))
-            model(view, memory, prompt, df, vectorstore)
-    elif view.selected_db:
-        # memory.chat_memory.messages = []
-        selected_db_path = os.path.join(DB_DIRECTORY, view.selected_db)
-        vectorstore = Chroma(persist_directory=selected_db_path, embedding_function=embeddings)
-        model(view, memory, prompt, df, vectorstore)
-
-    else:
-        raise ValueError("Either a PDF file or a selected DB must be provided...")
-
-
-STREAM = False
-
-
-def model(view, memory, prompt, df, db):
+def create_conversational_chain(view, memory, prompt, df, db):
     llm = ChatOpenAI(
         model_name=view.model_name,
         temperature=view.temperature,
@@ -234,12 +160,6 @@ def model(view, memory, prompt, df, db):
         presence_penalty=view.presence_penalty
     )
 
-    # conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm,
-    #                                                            retriever=db.as_retriever(),
-    #                                                            memory=memory,
-    #                                                            verbose=True,
-    #                                                            prompt=prompt
-    #                                                              )
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm,
         retriever=db.as_retriever(search_kwargs={"k": 3}),
@@ -256,24 +176,62 @@ def model(view, memory, prompt, df, db):
 
     if view.user_query:
         view.add_message(view.user_query, "user")
-        if STREAM:
-            st_callback = view.add_message_stream("assistant")
-            conversation_chain({"question": view.user_query}, callbacks=[st_callback])
-        else:
-            response = conversation_chain({"question": view.user_query})
-            view.add_message(response['answer'], "assistant")
+        response = conversation_chain({"question": view.user_query})
+        view.add_message(response['answer'], "assistant")
 
     elif view.csv_file:
         for index, row in df.iterrows():
             value = df.iloc[index, 0]
             view.add_message(value, "user")
-            if STREAM:
-                st_callback = view.add_message_stream("assistant")
-                conversation_chain({"question": value}, callbacks=[st_callback])
-            else:
-                response = conversation_chain({"question": value})
-                view.add_message(response['answer'], "assistant")
+            response = conversation_chain({"question": value})
+            view.add_message(response['answer'], "assistant")
 
 
-if __name__ == "__main__":
-    main()
+# Initialize the StreamlitChatView
+view = StreamlitChatView()
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "context", "question"],
+    template=view.context_prompt
+)
+
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, output_key='answer', return_messages=True)
+
+df = read_csv(view)
+
+col1, col2 = st.columns([6, 1])
+
+with col1:
+    st.header(f"Chat with Docs using OpenAI")
+
+with col2:
+    if st.button("Clear ↺"):
+        clear_chat(memory)
+
+
+if view.input_file:
+    data = prepare_docs(view)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents(data)
+    if os.path.exists(os.path.join(DB_DIRECTORY, view.input_file.name)):
+        st.warning('already have a db for this', icon="⚠️")
+    else:
+        embeddings = OpenAIEmbeddings(model=view.embeddings_model_name)
+        vectorstore = Chroma.from_documents(docs, embeddings)
+        if st.sidebar.button("save to database"):
+            # vectorstore.persist(persist_directory=os.path.join(DB_DIRECTORY, view.input_file.name))
+            vectorstore = Chroma.from_documents(docs, embeddings, persist_directory=os.path.join(DB_DIRECTORY, view.input_file.name))
+            st.success('database saved successfully select from below', icon="✅")
+        create_conversational_chain(view, memory, prompt, df, vectorstore)
+
+elif view.selected_db:
+    embeddings = OpenAIEmbeddings(model=view.embeddings_model_name)
+    selected_db_path = os.path.join(DB_DIRECTORY, view.selected_db)
+    vectorstore = Chroma(persist_directory=selected_db_path, embedding_function=embeddings)
+    create_conversational_chain(view, memory, prompt, df, vectorstore)
+elif view.selected_db and view.input_file:
+    st.warning("select only one source( Either upload or select db )", icon="⚠️")
+else:
+    st.info('select a database or upload file to create db', icon="ℹ️")
+
